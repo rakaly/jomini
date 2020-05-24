2642226b3d364f349fa5f376208d2e21e21377c9
Nick Babcock <nbabcock19@hotmail.com>
2020-05-24 07:36:28 -0500

    Update jomini example to not panic

--- examples/tape_binary.rs
+++ examples/tape_binary.rs
@@ -4,15 +4,11 @@ use std::io::{self, Read};
 fn main() -> Result<(), Box<dyn error::Error>> {
     let mut data = Vec::new();
     io::stdin().read_to_end(&mut data)?;
-    println!(
-        "{:#?}",
-        jomini::BinTape::from_slice(&data).unwrap().token_tape
-    );
-
-    /*    let mut parser = BinaryParser::new();
-    for event in parser.events(&data) {
-        println!("{:?}", event);
-    }*/
+    let res = jomini::BinTape::from_slice(&data);
+    match res {
+        Ok(t) => println!("{:#?}", t.token_tape),
+        Err(e) => println!("errored with {}", e),
+    }
 
     Ok(())
 }
