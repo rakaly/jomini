8fe554c84e51672cbcb75be3bce2d273eb67026b
Nick Babcock <nbabcock19@hotmail.com>
2020-05-27 20:22:04 -0500

    Remove deprecated unused modules

--- src/binary/mod.rs
+++ src/binary/mod.rs
@@ -1,15 +1,8 @@
 pub mod de;
-pub mod document;
 mod errors;
-mod parser;
-mod reader;
 mod resolver;
 mod tape;
 
 pub use self::errors::BinaryDeError;
-pub use self::parser::{
-    BinaryError, BinaryErrorKind, BinaryEvent, BinaryEventsIter, BinaryParseEvent, BinaryParser,
-};
-pub use self::reader::{BinaryReader, BinaryReaderBuilder, BinaryReadingError, EventRecord};
 pub use self::resolver::{FailedResolveStrategy, TokenResolver};
 pub use self::tape::{BinTape, BinaryToken};
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
 pub(crate) mod ascii;
 pub mod binary;
 mod data;
-pub mod de;
-pub(crate) mod depth;
-mod operator;
 mod scalar;
 pub mod text;
 
 pub use self::binary::*;
-pub use self::operator::*;
 pub use self::scalar::{Scalar, ScalarError};
 pub use self::text::*;
