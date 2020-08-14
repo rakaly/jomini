f9fd011fec6b772040c86598b075655d4612d4d0
nickbabcock <nbabcock19@hotmail.com>
2020-08-14 06:15:47 -0500

    TextTape parser supports rgb color values
    The binary parser already supports parsing RGB color info found in
    imperator saves, so the text parser should too.
    
    The format looks like the following:
    
    ```
    color = rgb { 100 200 150 }
    ```
    
    This does not implement deserialization
    
    Benchmarks did decrease. Latency to parse text increased by about 5%. I
    tried to lower this number through various mechanisms (like putting
    least used paths in another branch) but was unsuccessful.

--- src/lib.rs
+++ src/lib.rs
@@ -135,6 +135,7 @@ mod text;
 pub(crate) mod util;
 
 pub use self::binary::*;
+pub use self::data::Rgb;
 pub use self::errors::*;
 pub use self::scalar::{Scalar, ScalarError};
 pub use self::text::*;
