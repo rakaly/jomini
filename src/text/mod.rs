a9a666480f336840d0bb1aa4c66b64b60dd66149
Nick Babcock <nbabcock19@hotmail.com>
2021-05-25 21:15:45 -0500

    Add write API
    Previously we've only concerned ourselves with the ability to read. This
    commit adds the ability to write data out. We've already needed the
    ability to write in downstream melters and formatters but it was deemed
    easy enough to just manually write it. This all changed when I learned
    that binary quoted values are not stored in their escaped form. Escaping
    is needed to properly store a name like:
    
    ```
    Captain "Biggie" Smalls
    ```
    
    The quotes in the above example are stored as is in the binary format
    but need to be escaped when melted. Duplicating the escaping logic to
    all the downstream projects seems like a chore and bug prone, so I
    decided to create a write API that both formatters and melters can use.
    
    Formatters and melters are different use cases so they use different
    APIs. The formatting use case is the easiest to visualize: text is read
    and text is written:
    
    ```rust
    let tape = TextTape::from_slice(b"hello   = world")?;
    let mut out: Vec<u8> = Vec::new();
    let mut writer = TextWriterBuilder::new().from_writer(&mut out);
    writer.write_tape(&tape)?;
    assert_eq!(&out, b"hello=world\n");
    ```
    
    The writer normalizes any formatting issues. The writer is not able to
    losslessly write all parsed documents, but these are limited to truly
    esoteric situations and hope to be resolved in future releases.
    
    The other API is geared more towards the melters or those crafting
    documents by hand. These use cases need to manually drive the writer:
    
    ```rust
    let mut out: Vec<u8> = Vec::new();
    let mut writer = TextWriterBuilder::new().from_writer(&mut out);
    writer.write_unquoted(b"hello")?;
    writer.write_unquoted(b"world")?;
    writer.write_unquoted(b"foo")?;
    writer.write_unquoted(b"bar")?;
    assert_eq!(&out, b"hello=world\nfoo=bar\n");
    ```

--- src/text/mod.rs
+++ src/text/mod.rs
@@ -2,8 +2,10 @@
 mod de;
 mod reader;
 mod tape;
+mod writer;
 
 #[cfg(feature = "derive")]
 pub use self::de::TextDeserializer;
 pub use self::reader::{ArrayReader, ObjectReader, Reader, ScalarReader, ValueReader};
 pub use self::tape::{Operator, TextTape, TextToken};
+pub use self::writer::*;
