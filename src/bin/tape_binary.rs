2f66845203cd469246ece3c454b114a4abe9093b
Nick Babcock <nbabcock19@hotmail.com>
2022-10-18 17:40:27 -0500

    Move examples to bin directory
    It's annoying that the examples directory requires compilation of dev
    dependencies. I was doing some benchmarking on a slower computer and I
    much prefer to be able to run with `--no-default-features` and only need
    to compile the actual crate + example. Saves about a minute each time a
    need to do an iteration when benchmarking large data that doesn't fit in
    the repo.
    
    I also updated the output so that it showed elapsed time to better
    exclude the time it takes to ingest stdin

--- src/bin/tape_binary.rs
+++ src/bin/tape_binary.rs
@@ -1,11 +1,18 @@
 use std::error;
 use std::io::{self, Read};
+use std::time::Instant;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
+    let start = Instant::now();
     let mut data = Vec::new();
     io::stdin().read_to_end(&mut data)?;
+    println!("ingest: {}ms", start.elapsed().as_millis());
+    let read = Instant::now();
+
     match jomini::BinaryTape::from_slice(&data) {
-        Ok(t) => println!("{:#?}", t.tokens()),
+        Ok(_) => {
+            println!("parse: {}ms", read.elapsed().as_millis())
+        }
         Err(e) => println!("errored with {}", e),
     }
 
