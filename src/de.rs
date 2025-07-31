6e1f6cdb77bc4de52e73a62218953d54654f8af3
Nick Babcock <nbabcock19@hotmail.com>
2025-07-31 07:07:58 -0500

    Omit rgb header when deserializing binary colors

--- src/de.rs
+++ src/de.rs
@@ -12,55 +12,6 @@ impl ColorSequence {
     pub(crate) fn new(data: Rgb) -> Self {
         ColorSequence { data, idx: 0 }
     }
-}
-
-impl<'de> de::Deserializer<'de> for &'_ mut ColorSequence {
-    type Error = Error;
-
-    fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>,
-    {
-        if self.idx == 1 {
-            visitor.visit_borrowed_str("rgb")
-        } else {
-            visitor.visit_seq(InnerColorSequence::new(self.data))
-        }
-    }
-
-    serde::forward_to_deserialize_any! {
-        bool i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 char str string
-        bytes byte_buf option unit unit_struct newtype_struct seq tuple
-        tuple_struct map struct enum identifier ignored_any
-    }
-}
-
-impl<'de> SeqAccess<'de> for ColorSequence {
-    type Error = Error;
-
-    fn next_element_seed<T>(&mut self, seed: T) -> Result<Option<T::Value>, Self::Error>
-    where
-        T: DeserializeSeed<'de>,
-    {
-        if self.idx >= 2 {
-            Ok(None)
-        } else {
-            self.idx += 1;
-            seed.deserialize(self).map(Some)
-        }
-    }
-}
-
-#[derive(Debug)]
-pub(crate) struct InnerColorSequence {
-    data: Rgb,
-    idx: usize,
-}
-
-impl InnerColorSequence {
-    pub(crate) fn new(data: Rgb) -> Self {
-        InnerColorSequence { data, idx: 0 }
-    }
 
     fn val(&self) -> u32 {
         match self.idx {
@@ -73,7 +24,7 @@ impl InnerColorSequence {
     }
 }
 
-impl<'de> de::Deserializer<'de> for &'_ mut InnerColorSequence {
+impl<'de> de::Deserializer<'de> for &'_ mut ColorSequence {
     type Error = Error;
 
     fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
@@ -90,7 +41,7 @@ impl<'de> de::Deserializer<'de> for &'_ mut InnerColorSequence {
     }
 }
 
-impl<'de> SeqAccess<'de> for InnerColorSequence {
+impl<'de> SeqAccess<'de> for ColorSequence {
     type Error = Error;
 
     fn next_element_seed<T>(&mut self, seed: T) -> Result<Option<T::Value>, Self::Error>
--- tests/de.rs
+++ tests/de.rs
@@ -5,7 +5,7 @@ use jomini::{
     Windows1252Encoding,
 };
 use serde::{
-    de::{self, Visitor},
+    de::{self},
     Deserialize, Deserializer,
 };
 use std::collections::HashMap;
@@ -37,17 +37,10 @@ impl Encoding for BinaryTestFlavor {
 }
 
 #[test]
-fn same_deserializer_for_header_token() {
+fn unified_rgb_deserializer() {
     #[derive(Deserialize, Debug, PartialEq)]
     struct MyStruct {
-        color: Color,
-    }
-
-    #[derive(Debug, PartialEq)]
-    struct Color {
-        red: u8,
-        blue: u8,
-        green: u8,
+        color: [u8; 3],
     }
 
     let bin_data = [
@@ -58,7 +51,7 @@ fn same_deserializer_for_header_token() {
     let mut map = HashMap::new();
     map.insert(0x053a, "color");
 
-    let txt_data = b"color = rgb { 110 27 27 }";
+    let txt_data = b"color = { 110 27 27 }";
 
     let bin_out: MyStruct = BinaryDeserializer::builder_flavor(BinaryTestFlavor)
         .deserialize_slice(&bin_data[..], &map)
@@ -68,47 +61,9 @@ fn same_deserializer_for_header_token() {
     assert_eq!(
         bin_out,
         MyStruct {
-            color: Color {
-                red: 110,
-                blue: 27,
-                green: 27,
-            }
+            color: [110, 27, 27]
         }
     );
-
-    impl<'de> Deserialize<'de> for Color {
-        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-        where
-            D: Deserializer<'de>,
-        {
-            struct ColorVisitor;
-
-            impl<'de> Visitor<'de> for ColorVisitor {
-                type Value = Color;
-
-                fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                    formatter.write_str("a color")
-                }
-
-                fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
-                where
-                    A: de::SeqAccess<'de>,
-                {
-                    let ty = seq.next_element::<&str>()?.expect("value type");
-                    match ty {
-                        "rgb" => {
-                            let (red, green, blue) =
-                                seq.next_element::<(u8, u8, u8)>()?.expect("rgb channels");
-                            Ok(Color { red, green, blue })
-                        }
-                        _ => panic!("unexpected color type"),
-                    }
-                }
-            }
-
-            deserializer.deserialize_seq(ColorVisitor)
-        }
-    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
